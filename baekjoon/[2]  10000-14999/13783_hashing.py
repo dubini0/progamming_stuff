d21810c8ef8f48de8ae296abe13af58c94405424
Subin Choi <50950237+dubini0@users.noreply.github.com>
2021-12-28 05:03:34 +0900

    Solved memory issue

--- baekjoon/[2]  10000-14999/13783_hashing.py
+++ baekjoon/[2]  10000-14999/13783_hashing.py
@@ -1,6 +1,3 @@
-# NOT DONE YET!
-# WORKING ON IT, HAVING MEMEORY ISSUES
-
 from itertools import product
 
 def get_hash(n_list):
@@ -10,18 +7,24 @@ def get_hash(n_list):
         hash_val += n_list[i]*(31**(n-i-1))
     return hash_val % 1000000007
 
-def get_hash_crash(hash_val):
-    string_len, result = 1, 0
-    while True:
-        min_val = sum([31**i for i in range(string_len)])
+def get_max_string_len(hash_val):
+    for string_len in range(30):
+        min_val = sum([31**i for i in range(string_len+1)])
         if min_val > hash_val:
-            return result
-        n_list = [i for i in product(list(range(32, 127)), repeat=string_len)]
+            return string_len
+
+def get_hash_crash(hash_val):
+    result = 0
+    string_len = get_max_string_len(hash_val)
+    #print(string_len)
+    for j in range(string_len):
+        n_list = [i for i in product(list(range(32, 127)), repeat=j)]
         for i in range(len(n_list)):
             hash_cal = get_hash(list(n_list[i]))
             if hash_cal == hash_val:
                 result += 1
-        string_len += 1
+
+    return result
 
 if __name__ == "__main__":
     while True:
@@ -31,5 +34,5 @@ if __name__ == "__main__":
         del n_list[0]
         hash_val = get_hash(n_list)
         result = get_hash_crash(hash_val)
-        #print(hash_val)
+        # print(f"result : {result}")
         print(result)
