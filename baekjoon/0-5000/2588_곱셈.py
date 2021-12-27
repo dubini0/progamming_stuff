fc15dc53507db45e3b6e28ca39bb2dfee67d3e47
Subin Choi <50950237+dubini0@users.noreply.github.com>
2021-12-27 21:02:50 +0900

    Upload 2588_곱셈.py

---
+++ baekjoon/0-5000/2588_곱셈.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = input()
+
+one_digit = a * int(b[2])
+ten_digit = a * int(b[1])
+hun_digit = a * int(b[0])
+result = a * int(b)
+
+print(one_digit)
+print(ten_digit)
+print(hun_digit)
+print(result)
