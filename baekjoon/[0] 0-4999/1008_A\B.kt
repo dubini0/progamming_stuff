a5b826410d848310d6077028cffc0b7c69ffd2f1
Subin Choi <50950237+dubini0@users.noreply.github.com>
2022-03-04 22:13:22 +0900

    Upload 1008_A\B.kt

---
+++ baekjoon/[0] 0-4999/1008_A\B.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinpractice
+
+import java.util.Scanner
+
+fun main() {
+    var sc :Scanner = Scanner(System.`in`)
+    var a :Double = sc.nextDouble()
+    var b :Double = sc.nextDouble()
+
+    print(a/b)
+}
