ba97d4d78b3b430505d1357f0cb16c3794e6ba74
Subin Choi <50950237+dubini0@users.noreply.github.com>
2022-03-08 02:54:11 +0900

    minor changes

--- baekjoon/[0] 0-4999/2739_구구단.kt
+++ baekjoon/[0] 0-4999/2739_구구단.kt
@@ -2,9 +2,9 @@ import java.io.*
 
 fun main() {
     val br :BufferedReader = BufferedReader(InputStreamReader(System.`in`))
-    val n = br.readLine()
+    val n = br.readLine().toInt()
 
     for (i in 1..9){
-        println("$n * $i = ${2*i}")
+        println("$n * $i = ${n*i}")
     }
 }
