9eef91b4a0299e5687492f9533f50b4cedaeb5e1
Subin Choi <50950237+dubini0@users.noreply.github.com>
2022-03-21 22:08:11 +0900

    minor changes

--- baekjoon/[2]  10000-14999/11729_하노이 탑 이동 순서.kt
+++ baekjoon/[2]  10000-14999/11729_하노이 탑 이동 순서.kt
@@ -1,9 +1,15 @@
-fun main() {
-    val sb = StringBuilder()
-    val n = readLine()!!.toInt()
-    var cnt = 0
-  
-    hanoi(n, 1,2,3)
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.util.*
+
+val sb = StringBuilder()
+var cnt = 0
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    var n = readLine().toInt()
+
+    hanoi(n, 1, 2, 3)
     println(cnt)
     print(sb)
 }
@@ -11,7 +17,7 @@ fun main() {
 fun hanoi(num: Int, start: Int, sub: Int, to: Int){
     if (num == 0) return
     cnt += 1
-    hanoi (num - 1, start, to, sub) // N - 1 개 원반을 시작점 -> sub
-    sb.append("$start $to\n") // n번 째 원반 -> 목표 기둥
-    hanoi(num - 1, sub, start, to) // N-1 개 원반 sub -> to
+    hanoi(num-1, start, to, sub)
+    sb.append("$start $to\n")
+    hanoi(num-1, sub, start, to)
 }
