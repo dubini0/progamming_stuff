da906047065a1296d2a424ae05fd0b5722ed22e3
Subin Choi <50950237+dubini0@users.noreply.github.com>
2022-03-25 00:10:49 +0900

    error fix - 2309_일곱난쟁이.py

--- baekjoon/[0] 0-4999/2309_일곱난쟁이.py
+++ baekjoon/[0] 0-4999/2309_일곱난쟁이.py
@@ -1,18 +1,14 @@
-
-input_list = []
-for i in range(9):
-    input_list.append(int(input()))
-
-find = sum(input_list) - 100
-for i in range(len(input_list)-1):
-    for j in range(i+1, len(input_list)):
-        # print(i, j)
-        if (input_list[i] + input_list[j]) == find:
-            a, b = input_list[i], input_list[j]
-            input_list.remove(a)
-            input_list.remove(b)
-            break
-
-input_list.sort()
-for i in input_list:
-    print(i)
+n = 9
+temp1, temp2 = 0, 0
+arr = [int(input()) for _ in range(n)]
+ 
+for i in range(n):
+    for j in range(i+1, n):
+        if sum(arr) - (arr[i] + arr[j]) == 100:
+            temp1 = arr[i]
+            temp2 = arr[j]
+ 
+arr.remove(temp1)
+arr.remove(temp2)
+ 
+print('\n'.join(map(str, sorted(arr))))
