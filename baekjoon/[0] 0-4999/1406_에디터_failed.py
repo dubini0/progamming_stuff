37b0ac7f1258fc188cc5b8136503d3d016e53ec4
Subin Choi <50950237+dubini0@users.noreply.github.com>
2022-05-16 18:25:17 +0900

    initial commit - 1406_에디터_failed.py

---
+++ baekjoon/[0] 0-4999/1406_에디터_failed.py
@@ -0,0 +1,33 @@
+# Note) This code works fine, but it does not satisfy the time limit.
+# If you want to see the 'fast' code, see 1406_에디터_success.py
+
+import sys
+
+string = sys.stdin.readline().strip()
+n = int(sys.stdin.readline())
+index = len(string)
+
+for i in range(n):
+    args = sys.stdin.readline().split()
+    # print(args)
+    if args[0] == "P":
+        string = string[:index] + args[1] + string[index:]
+        index += 1
+    elif args[0] == "L":
+        if index == 0:
+            continue
+        else:
+            index -= 1
+    elif args[0] == "D":
+        if index == len(string):
+            continue
+        else:
+            index += 1
+    elif args[0] == "B":
+        if index == 0:
+            continue
+        string = string[:index-1] + string[index:]
+        index -= 1
+    #print(string)
+
+print(string)
